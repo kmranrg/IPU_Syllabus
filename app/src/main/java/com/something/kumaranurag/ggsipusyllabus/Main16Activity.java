1e2c454ae89611912951271403829d2cf1b05e58
Kumar Anurag <kmranrg@gmail.com>
2019-03-30 12:00:13 +0530

    enhanced dev page

--- app/src/main/java/com/something/kumaranurag/ggsipusyllabus/Main16Activity.java
+++ app/src/main/java/com/something/kumaranurag/ggsipusyllabus/Main16Activity.java
@@ -3,20 +3,17 @@ package com.something.kumaranurag.ggsipusyllabus;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.github.barteksc.pdfviewer.PDFView;
+
 
 /**
  * Created by Kumar Anurag on 7/14/2017.
  */
 
 public class Main16Activity extends AppCompatActivity {
-    PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        pdfView= (PDFView)findViewById(R.id.pdfView);
-        pdfView.fromAsset("dev.pdf").load();
+        setContentView(R.layout.activity_main3);
     }
 }
